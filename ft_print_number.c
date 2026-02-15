54023ca29a9989e45f60eeeed6ac3cac65fbcbab
ebedshahad903-max <ebedshahad903@gmail.com>
2026-02-15 12:49:37 +0400

    final

--- ft_print_number.c
+++ ft_print_number.c
@@ -1,5 +1,6 @@
 #include "ft_printf.h"
-
+//4567 -> n/10 = 456 /)7 
+// 
 static int  putnbr(long n)
 {
     int count = 0;
@@ -10,12 +11,21 @@ static int  putnbr(long n)
         n = -n;
     }
     if (n >= 10)
-        count += putnbr(n / 10);
-    count += print_char((n % 10) + '0');
+        count += putnbr(n / 10);//: 4567 -> 456-> 45 ->4 
+
+    count+= print_char((n % 10) + '0');//: 4567 %10 = = 7 +0 '7'
     return (count);
 }
 
 int print_number(int n)
 {
     return (putnbr((long)n));
+}
+int main ()
+
+{
+    int a =10;
+    char b = 30;
+    write
+    return 0;
 }
